8f89bbbf3ba880ed36b36ed15b4859a7e040d0aa
nirmalh <nirmalh@qburst.com>
2018-10-29 17:09:06 +0530

    devise model for user is created(fields are: email and password)

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules.
+  # :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  # Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end

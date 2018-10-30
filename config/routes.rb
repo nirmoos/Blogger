f8726eff7df8390f78d5931b250cc28d894d3cf8
nirmalh <nirmalh@qburst.com>
2018-10-30 17:42:11 +0530

    articles and comments controller created and routes defined.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
+
+  root to: 'articles#index'
+
+  resources :articles do
+    resources :comments, shallow: true
+  end
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 

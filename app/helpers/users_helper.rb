c3cb5a609cf4a1a06ac72136afc4f589ca2d05d8
nirmalh <nirmalh@qburst.com>
2018-11-18 19:47:55 +0530

    user profile created as sidebar.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,10 @@ module UsersHelper
     if user.avatar.attached?
         image_tag user.avatar
     else
-        image_tag 'default_image.jpg'
+        image_tag 'default_image.png'
     end
+  end
+  def user_cover(user_id)
+    image_tag 'default_cover.jpeg'
   end
 end
